051aa2abb71e120ef4ab51871f2eda5b25671014
Marques Lee <marques.lee@thoughtworks.com>
2019-04-24 11:01:55 -0700

    and of course, the import

--- server/webapp/WEB-INF/rails/webpack/models/pipeline_configs/pipeline_config.ts
+++ server/webapp/WEB-INF/rails/webpack/models/pipeline_configs/pipeline_config.ts
@@ -21,7 +21,7 @@ import {Stream} from "mithril/stream";
 import * as stream from "mithril/stream";
 import {Material} from "models/materials/types";
 import {ValidatableMixin} from "models/mixins/new_validatable_mixin";
-import {NameableSet, NonEmptySetValidator} from "./material_set";
+import {NameableSet, NonEmptySetValidator} from "./nameable_set";
 import {Stage} from "./stage";
 
 export class PipelineConfig extends ValidatableMixin {
